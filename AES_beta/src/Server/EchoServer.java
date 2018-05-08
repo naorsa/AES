0ad1a38f256b163fbebc2cdba4b9ff5daf9b8f71
Omer Haimovich <omer16493@gmail.com>
2018-05-08 12:24:37 +0300

    Server without database

--- AES_beta/src/Server/EchoServer.java
+++ AES_beta/src/Server/EchoServer.java
@@ -36,7 +36,7 @@ public class EchoServer extends AbstractServer
 	    String str = (String) msg;
 	    if(str.equals("Data base"))
 	    {
-	    	str ="Moshe";
+	    	str ="Mosheh";
 	    }
 	    this.sendToAllClients(msg);
 	  }
