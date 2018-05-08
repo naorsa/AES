57655e0b7c4640d1c11f507c1807cb0298b8f33a
Omer Haimovich <omer16493@gmail.com>
2018-05-08 12:21:44 +0300

    Try

--- AES_beta/src/Server/EchoServer.java
+++ AES_beta/src/Server/EchoServer.java
@@ -36,7 +36,7 @@ public class EchoServer extends AbstractServer
 	    String str = (String) msg;
 	    if(str.equals("Data base"))
 	    {
-	    	str ="";
+	    	str ="Moshe";
 	    }
 	    this.sendToAllClients(msg);
 	  }
